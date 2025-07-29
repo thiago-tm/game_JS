d3e19f8de4e4ce50d9fc28caddf04523cd59bd63
Thiago Marcelino | LAFORT <thiago.marcelino@lafortmalhas.local>
2025-07-29 15:58:59 -0300

    firt commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto'
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero de 1 a 100';

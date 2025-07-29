74fbf71bdd0d42f4fe7ff3d2b0a9f124e169f859
Thiago Marcelino | LAFORT <thiago.marcelino@lafortmalhas.local>
2025-07-29 16:44:55 -0300

    logica game

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto'
+let numeroSecreto = numeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero de 1 a 100';
+function exibirNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirNaTela('h1', 'Jogo do Número Secreto');
+exibirNaTela('p', 'Escolha um numero de 1 a 10');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+   
+    if (chute == numeroSecreto) {
+        exibirNaTela('h1', 'Acertou!');
+
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+
+        exibirNaTela('p', `${mensagemTentativas}`);
+    } else{
+        if (chute > numeroSecreto) {
+            exibirNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirNaTela('p', 'O número secreto é maior');  
+        }
+        
+        tentativas++;
+    }
+}
+
+function numeroAleatorio() {
+   return parseInt(Math.random() * 10 + 1);
+}
